1e2fc983cb6f165dfaecfed4d8112343a5c1b047
Nick H <manifes7o@gmail.com>
2018-01-01 17:02:31 -0500

    Ran the driver for a couple years and pickled the data

--- data/scrapedata.py
+++ data/scrapedata.py
@@ -1,13 +1,25 @@
-from itertools import islice
 import os
+import sys
+import pickle
+from itertools import islice
 
 from movieDataScraper.driver import Driver
 
+# otherwise pickle throws a fit trying to pack classes
+sys.setrecursionlimit(9999)
+
 base = r'./bestpicturesbyyear/'
+allyears = [x[2] for x in os.walk(base)][0]
 
-allyears = [x[2] for x in os.walk('./bestpicturesbyyear')][0]
+alldata = []
 
-for yearFile in islice(allyears, 0, 2):
+for yearFile in islice(allyears, 0, 1):
     with open(base + yearFile, 'r') as f:
         for movie in f:
             print(movie)
+            driver = Driver(movie)
+            alldata.append((yearFile, movie.strip(), driver.bom))
+
+p = open('bestpictures.pkl', 'wb')
+p.dump(alldata, p)
+p.close()
